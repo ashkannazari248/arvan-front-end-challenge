fba6079b10d2f3b84ea9decf971cacaf851df8e3
ict-site1 <ict-site1@ICT-SITE1>
2022-10-11 11:29:59 +0330

    create auth layout

---
+++ src/layouts/LayoutAuth.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container d-flex">
+    <div class="col-12 justify-content-center align-self-center">
+      <div id="layout_auth" class="align-middle col-lg-4 col-md-6 col-sm-8 col-12 mx-auto my-auto border border-secondary rounded p-3">
+        <slot/>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+.container {
+  min-height: 100vh !important;
+}
+
+#layout_auth {
+  background-color: var(--silver) !important;
+}
+
+</style>
+
+
+
