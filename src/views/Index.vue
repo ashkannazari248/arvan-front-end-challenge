7f26a194db07549b4c24a2205373e53ce4c04d47
ict-site1 <ict-site1@ICT-SITE1>
2022-10-11 11:54:57 +0330

    create index page for show all routes link

---
+++ src/views/Index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import {useRouter} from "vue-router";
+
+const router=useRouter()
+</script>
+
+<template>
+  <main>
+    <ul class="list-unstyled p-0 d-flex flex-row flex-wrap"><li class="p-0 m-0" v-for="route in router.options.routes"><RouterLink class="btn btn-primary m-3"  :to="route.path">{{route.name}}</RouterLink></li></ul>
+  </main>
+</template>
