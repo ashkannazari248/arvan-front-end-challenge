fd3364c031f9d61810bf3021e57e71b001077675
ict-site1 <ict-site1@ICT-SITE1>
2022-10-11 11:17:00 +0330

    add pinia to project

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { createApp } from 'vue'
 
 import App from '@/App.vue'
 import router from '@/router'
+import {createPinia} from 'pinia'
 
 import '@/assets/main.scss'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 
 
 const app = createApp(App)
+app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
