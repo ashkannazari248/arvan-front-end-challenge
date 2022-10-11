bf2861b74a2147efeb94a20926a27b4c27d14d83
ict-site1 <ict-site1@ICT-SITE1>
2022-10-11 11:18:04 +0330

    create defult button component

---
+++ src/components/AppButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <button :class="{ disabled:props.loading, [`btn btn-${props.variant}`]: props.variant !== undefined }">
+    <slot/>
+    <span v-if="props.loading" class="spinner-border" role="status">
+      <i class="sr-only"/>
+    </span>
+
+  </button>
+</template>
+
+<script setup lang="ts">
+enum ButtonVariantEnum {
+  primary = 'primary',
+  secondary = 'secondary',
+  accent = 'accent',
+  ghost = 'ghost',
+  link = 'link'
+}
+
+interface Props {
+  variant?: ButtonVariantEnum;
+  loading?: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  loading: false,
+});
+</script>
+<style scoped lang="scss">
+.spinner-border{
+  width: 16px;
+  height: 16px;
+}
+button{
+  font-size: 16px;
+}
+</style>
