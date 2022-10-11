de189e76532c3ae526b5a1ecf8a5d6c9e58d92cf
ict-site1 <ict-site1@ICT-SITE1>
2022-10-11 11:26:32 +0330

    create default layout components

---
+++ src/components/layouts/default/DefaultHeader.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+    <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Company name</a>
+    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse"
+            data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <RouterLink class="navbar-nav px-3 py-1 text-decoration-none" id="logout" to="/logout">Logout</RouterLink>
+
+  </nav>
+</template>
+
+
+<style scoped lang="scss">
+.navbar-brand{
+  font-size: 22px;
+}
+
+
+
+#logout {
+  color: var(--dark-sky-blue);
+  border: 1px solid var(--dark-sky-blue);
+  border-radius: 6px;
+  margin: 0 15px;
+}
+</style>
+
---
+++ src/components/layouts/default/DefaultSidebar.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import {useRoute} from "vue-router";
+
+const route=useRoute()
+</script>
+
+<template>
+  <nav id="sidebarMenu" class="col-md-3 col-lg-2 p-0 d-md-block sidebar collapse">
+    <div class="sidebar-sticky pt-3">
+
+
+      <h6 class="sidebar-heading align-items-center ps-1 mt-4 mb-2 ">
+        <span>Articles</span>
+      </h6>
+      <ul class="nav flex-column mb-2">
+        <li class="nav-item">
+          <RouterLink
+              :class="['nav-link',{'router-link-active router-link-exact-active':route.name==='articles-page' || route.name==='articles'}]"
+              :to="{name:'articles'}" v-text="`Posts`"/>
+        </li>
+        <li class="nav-item">
+          <RouterLink exact class="nav-link" :to="{name:'article-create'}" v-text="`Create Post`"/>
+        </li>
+
+      </ul>
+    </div>
+  </nav>
+</template>
+
+
+<style scoped lang="scss">
+.navbar-brand, .sidebar-heading {
+  font-size: 22px;
+}
+
+.nav-link {
+  font-size: 18px;
+}
+
+#sidebarMenu {
+  background-color: var(--blue);
+  min-height: calc(100vh - 42px);
+
+  * {
+    color: #ffffff
+  }
+
+  a.router-link-active {
+    background-color: rgba(255, 255, 255, 0.15)
+  }
+}
+
+</style>
+
