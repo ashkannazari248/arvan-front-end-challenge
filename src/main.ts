4f59b4750bc61681711e72897e803b41d1ddd246
ict-site1 <ict-site1@ICT-SITE1>
2022-10-11 11:12:45 +0330

    add sass and config files

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { createApp } from 'vue'
+
 import App from '@/App.vue'
 
+import '@/assets/main.scss'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+
+
 const app = createApp(App)
+
 
 app.mount('#app')
 
