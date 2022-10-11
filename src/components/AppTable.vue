e07f585435e9592bb8e39c897244916899808627
ict-site1 <ict-site1@ICT-SITE1>
2022-10-11 11:22:44 +0330

    create default table component

---
+++ src/components/AppTable.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <table class="table AppTable">
+    <thead>
+    <slot name="thead"/>
+    </thead>
+    <tbody>
+    <slot name="tbody"/>
+    </tbody>
+  </table>
+</template>
+
+
+<style scoped lang="scss">
+.AppTable {
+  thead {
+    background-color: var(--silver);
+  }
+  tr {
+    line-height: min(40px);
+  }
+}
+
+</style>
+
